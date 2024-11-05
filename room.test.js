9e163a2c034bf4f06da03937743237ca92e66ca8
Sydney Goeury <goeurysydney@gmail.com>
2024-11-05 09:49:44 +0100

    should fail

--- room.test.js
+++ room.test.js
@@ -2,5 +2,5 @@ const Room = require('./room');  // Import Room correctly
 
 test('Length of the array types', () => {
     const currentRoom = new Room();
-    expect(currentRoom.types.length).toBe(3);
+    expect(currentRoom.types.length).toBe(4);
 });
